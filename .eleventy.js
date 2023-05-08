6c5b5f40c1ee0e92dced3fadaa224d27f9e58be9
Saptak S <saptak013@gmail.com>
2023-05-08 19:45:33 +0530

    Adds pathprefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("design-system");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("documents");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("keybase.txt");
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 };
